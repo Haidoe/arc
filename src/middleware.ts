3d12c8b54317e1a8f7e1189ea440958d06a445c7
haidoe <haidren.amalia@gmail.com>
2023-05-13 21:25:43 -0700

    Added Clerk and Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Kaos 🌚</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
