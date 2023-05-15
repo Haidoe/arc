23eaebc560481360916f3896eea9201dc4439e06
haidoe <haidren.amalia@gmail.com>
2023-05-15 14:59:16 -0700

    Added Middleware -- For Public and Private Routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,30 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-export default withClerkMiddleware(() => {
+// Set the paths that don't require the user to be signed in
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default withClerkMiddleware((req: NextRequest) => {
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
   return NextResponse.next();
 });
 
