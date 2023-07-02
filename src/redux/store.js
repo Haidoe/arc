aa488d77d40f04b3a77d04204f8768f1ca81925c
JB Sinluenam <jb.sinluenam@gmail.com>
2023-07-01 17:34:22 -0700

    feat: #KAOS-204 Add dummy reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    counter: counterReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
