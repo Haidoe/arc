ab593a2b1357bf433e3a0b7b7251a70ec1649b2c
haidoe <haidren.amalia@gmail.com>
2023-07-03 18:04:36 -0700

    feat: #KAOS-184 - add default avatar component

---
+++ src/components/global/DefaultAvatar.tsx
@@ -0,0 +1,9 @@
+const DefaultAvatar = () => {
+  return (
+    <div className="m-0 h-[88px] w-[88px] rounded-full bg-primary-base">
+      <span className="sr-only">Default avatar</span>
+    </div>
+  );
+};
+
+export default DefaultAvatar;
