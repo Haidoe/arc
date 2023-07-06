a4a851c66085939cd9033ccfde9c1229c51f0a03
JB Sinluenam <jb.sinluenam@gmail.com>
2023-07-05 21:23:58 -0700

    feat: #KAOS-201 Radio Input Field

--- src/components/RadioInputField.js
+++ src/components/RadioInputField.js
@@ -16,12 +16,12 @@ const RadioInputField = ({ enabled, onChange }) => {
       onChange={handleToggle}
       className={`${
         isEnabled ? "bg-primary-base" : "bg-contrast-light" // Updated state variable name
-      } relative inline-flex h-6 w-11 items-center rounded-full`}
+      } relative inline-flex h-6 w-12 items-center rounded-full transition-colors duration-300`}
     >
       <span
         className={`${
-          isEnabled ? "translate-x-6" : "translate-x-1" // Updated state variable name
-        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          isEnabled ? "translate-x-[26px]" : "translate-x-[2px]" // Updated state variable name
+        } inline-block h-5 w-5 rounded-full bg-white transition-transform duration-300 ease-in-out `}
       />
     </Switch>
   );
