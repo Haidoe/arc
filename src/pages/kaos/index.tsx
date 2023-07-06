c080dfa4c3bc76f63b2b97dd0e8259049363ab7f
haidoe <haidren.amalia@gmail.com>
2023-07-06 12:41:51 -0700

    feat: #KAOS-186 - add sample usage of modal

--- src/pages/kaos/index.tsx
+++ src/pages/kaos/index.tsx
@@ -4,11 +4,22 @@ import MainPageLayout from "~/components/layouts/MainPageLayout";
 import Sidebar from "~/components/production/Information";
 import { getProductionInfoById } from "~/service/production";
 import type { ProductionWithProducer } from "~/types/types";
+import Modal from "~/components/Modal";
+import Button from "~/components/Button";
 
 const Home: NextPage = () => {
   const [data, setData] = useState<ProductionWithProducer | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   useEffect(() => {
     getProductionInfoById("648fe91b5a6933035f1b9ab2")
@@ -22,15 +33,26 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <MainPageLayout>
-      <div className="flex flex-1">
-        <aside className="flex  flex-col md:basis-[384px]">
-          <Sidebar data={data} theme="primary" />
-        </aside>
+    <>
+      <MainPageLayout>
+        <div className="flex flex-1">
+          <aside className="flex  flex-col md:basis-[384px]">
+            <Sidebar data={data} theme="primary" />
+          </aside>
 
-        <div className="flex-grow bg-tertiary-light">Hello world</div>
-      </div>
-    </MainPageLayout>
+          <div className="flex-grow bg-tertiary-light">
+            <span>Hello world</span>
+            <Button buttonType="Primary" onClick={openModal}>
+              Modal
+            </Button>
+          </div>
+        </div>
+      </MainPageLayout>
+
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <div className="bg-primary-light">Hello world</div>
+      </Modal>
+    </>
   );
 };
 
