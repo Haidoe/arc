b0989743036e849a35c1f6f5fc7099f84a7297b2
JB Sinluenam <jb.sinluenam@gmail.com>
2023-07-07 20:17:14 -0700

    feat: #KAOS 212 Schedule of the day form-3

--- src/pages/production/[productionId]/report.js
+++ src/pages/production/[productionId]/report.js
@@ -28,6 +28,9 @@ const ProductionReportPage = () => {
         </aside>
 
         <div className="grid flex-grow grid-cols-2 gap-4 px-4">
+          <AccordionModal title="Schedule Of The Day" defaultOpen={true}>
+            <ScheduleOfTheDayForm />
+          </AccordionModal>
           <div className="bg-primary-light"> Form here </div>
           <div className="bg-primary-light"> Form here </div>
           <div className="bg-primary-light"> Form here </div>
