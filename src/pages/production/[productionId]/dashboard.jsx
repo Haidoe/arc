5e72fa3fa4c8ce7249690def1e935797b3351eb2
haidoe <haidren.amalia@gmail.com>
2023-07-08 16:13:13 -0700

    feat: #KAOS-190 - create initial dashboard report page

--- src/pages/production/[productionId]/dashboard.jsx
+++ src/pages/production/[productionId]/dashboard.jsx
@@ -1,14 +1,41 @@
-// read route and show header depending on route
-const Dashboard = () => {
+import MainPageLayout from "~/components/layouts/MainPageLayout";
+import Sidebar from "~/components/production/Information";
+import { getProductionInfoById } from "~/service/production";
+
+const ProductionReportPage = ({ productionInfo }) => {
   return (
-    <div>
-      <h1>This is the dashboard</h1>
-    </div>
+    <MainPageLayout>
+      <div className="flex  flex-1 bg-backgroundArc">
+        <aside className="flex  flex-shrink-0 flex-col bg-arc md:basis-[384px]">
+          <Sidebar data={productionInfo} theme="primary" />
+        </aside>
+
+        <div className="grid flex-grow grid-cols-2 gap-4 px-4 py-8">
+          <h2 className="text-3xl font-bold text-primary-dark">July 9, 2023</h2>
+        </div>
+      </div>
+    </MainPageLayout>
   );
 };
 
+export const getServerSideProps = async (ctx) => {
+  try {
+    const productionInfo = await getProductionInfoById(ctx.query.productionId);
 
+    return {
+      props: {
+        productionInfo,
+      },
+    };
+  } catch (error) {
+    //Redirect if the productionId is not valid
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+};
 
-
-// export header component
-export default Dashboard;
+export default ProductionReportPage;
