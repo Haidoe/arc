be230f94762ffa3390153b27b4e72a17cd338930
haidoe <haidren.amalia@gmail.com>
2023-07-08 01:24:48 -0700

    chore: prepare demo related pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { type NextPage } from "next";
-
+import type { NextPage, GetServerSideProps } from "next";
+import { clerkClient, getAuth } from "@clerk/nextjs/server";
 import { LoadingPage } from "~/components/Loading";
 import MainPageLayout from "~/components/layouts/MainPageLayout";
 
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
   return (
     <MainPageLayout>
-      <div className="flex flex-1 flex-col items-center justify-center px-4 text-center lg:px-0 xl:w-1/2">
+      <div className="flex flex-1 flex-col items-center justify-center px-4 text-center lg:px-0">
         <h1 className="mb-4 inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-4xl font-bold uppercase text-transparent">
           {data?.content}
         </h1>
@@ -21,12 +21,34 @@ const Home: NextPage = () => {
           Website is under maintenance. 🤕
         </p>
 
-        <p className="mt-4 text-xl uppercase text-primary-1">
+        <p className="text-primary-1 mt-4 text-xl uppercase">
           {`We'll be back soon`}
         </p>
       </div>
     </MainPageLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { userId } = getAuth(ctx.req);
+
+  const user = userId ? await clerkClient.users.getUser(userId) : undefined;
+
+  //Redirect to home if user is already logged in
+  if (user) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      userId: userId ? userId : null,
+    },
+  };
 };
 
 export default Home;
