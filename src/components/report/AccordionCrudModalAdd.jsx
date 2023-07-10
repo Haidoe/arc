292bc770a1f1c23de75b3ca4fe1e61500122a83e
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-07-10 16:00:08 -0700

    init: #KAOS-220 add accordion crud and basic modals setup

---
+++ src/components/report/AccordionCrudModalAdd.jsx
@@ -0,0 +1,17 @@
+// accordion crud add or update row modal
+
+const AccordionCrudModalAdd = ({ title, children, defaultOpen, modalWidth }) => {
+
+  // const castTimeLog = {}
+
+
+  return (
+    <>
+      <div>
+        Crud Modal Add
+      </div>
+    </>
+  )
+};
+
+export default AccordionCrudModalAdd;
