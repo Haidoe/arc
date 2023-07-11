65a80bf356e7630e261a446c36f42a8756178e3e
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-07-11 12:21:48 -0700

    fix: #KAOS-181 increase time component width

--- src/components/TimeInputField.js
+++ src/components/TimeInputField.js
@@ -7,7 +7,7 @@ const TimeInputField = ({ name, value, onChange, className, isReadyOnly }) => {
         name={name}
         value={value}
         onChange={onChange}
-        className={`rounded-[5px] border-[1px] border-contrast-light bg-arc px-2 text-[12px] text-hyperlink before:text-inherit ${className} focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-hyperlink `}
+        className={`rounded-[5px] border-[1px] border-contrast-light bg-arc px-2 text-[12px] text-hyperlink before:text-inherit ${className} focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-hyperlink w-[4.8rem] text-center`}
         readOnly={isReadyOnly}
       />
     </div>
