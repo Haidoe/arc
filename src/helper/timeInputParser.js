dfe78c18c40752338847f9d04c9a9c5aa5409dc4
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-07-14 17:22:21 -0700

    update: timeInput helper function to handle empty strings

--- src/helper/timeInputParser.js
+++ src/helper/timeInputParser.js
@@ -10,6 +10,8 @@ function timeStringToISO(timeString) {
 
   // Convert the date to UTC format
   const utcDateString = date.toISOString();
+
+  console.log(utcDateString)
   return utcDateString;
 }
 
@@ -20,16 +22,36 @@ function ISOToTimeString(utcDateString) {
   // Create a new Date object from the UTC date string
   const date = new Date(utcDateString);
 
-  // Get the hours and minutes from the Date object
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
+  // Convert to local time in Vancouver timezone
+  const vancouverTimeString = date.toLocaleString("en-US", {
+    timeZone: "America/Vancouver",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
 
-  // Create the time string in the desired format
-  const timeString = `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}`;
+  return hoursMinutesFormat(vancouverTimeString);
 
-  return timeString;
 }
+
+function hoursMinutesFormat(timeString) {
+  const timeParts = timeString.split(':');
+  let hours = parseInt(timeParts[0], 10);
+  const minutes = parseInt(timeParts[1].split(' ')[0], 10);
+
+  if (timeString.includes('PM') && hours !== 12) {
+    hours += 12;
+  } else if (timeString.includes('AM') && hours === 12) {
+    hours = 0;
+  }
+
+  const hours24Format = hours.toString().padStart(2, '0');
+  const hoursMinutesFormat = `${hours24Format}:${minutes.toString().padStart(2, '0')}`;
+
+  console.log(hoursMinutesFormat);
+  return hoursMinutesFormat;
+}
+
+
 
 export { timeStringToISO, ISOToTimeString };
