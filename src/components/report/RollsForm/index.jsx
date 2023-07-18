071de1b9591a6a1273dd171ab19f74c036e58093
JB Sinluenam <jb.sinluenam@gmail.com>
2023-07-18 10:29:48 -0700

    feat: #kaos-215-3-disable-tab-index

--- src/components/report/RollsForm/index.jsx
+++ src/components/report/RollsForm/index.jsx
@@ -49,76 +49,84 @@ const RollsForm = ({ }) => {
         <div onClick={() => setIsOpen(true)} action="" className={` text-contrast-dark`} >
           <div className="pb-2 border-b border-primary-base">
             <div className="grid grid-cols-6 grid-rows-2 gap-4 gap-y-2">
-              <p className="grid col-span-2">Script Supervisor</p>
-              <TextInputField className="grid col-span-4" placeholder="Name" value={scriptSupervisor} />
-              <p className="grid col-span-2">Data Wrangler</p>
-              <TextInputField className="grid col-span-4" placeholder="Name" value={dataWrangler} />
+              <p tabIndex="-1" className="grid col-span-2">Script Supervisor</p>
+              <TextInputField tabIndex="-1" className="grid col-span-4" placeholder="Name" value={scriptSupervisor} />
+              <p tabIndex="-1" className="grid col-span-2">Data Wrangler</p>
+              <TextInputField tabIndex="-1" className="grid col-span-4" placeholder="Name" value={dataWrangler} />
             </div>
           </div>
 
           <div className=" text-contrast-dark grid grid-cols-6 grid-rows-4 gap-4 gap-y-2 pt-2">
             <div className="grid col-span-2"></div>
-            {/* <div className="font-bold">A Cam</div>
-            <div className="font-bold">B Cam</div>
-            <div className="font-bold">C Cam</div>
-            <div className="font-bold">A Sound</div> */}
-            <p className="font-bold">A Cam</p>
-            <p className="font-bold">B Cam</p>
-            <p className="font-bold">C Cam</p>
-            <p className="font-bold">A Sound</p>
-            <p className="grid col-span-2 font-bold">Previously</p>
+            <p tabIndex="-1" className="font-bold">A Cam</p>
+            <p tabIndex="-1" className="font-bold">B Cam</p>
+            <p tabIndex="-1" className="font-bold">C Cam</p>
+            <p tabIndex="-1" className="font-bold">A Sound</p>
+            <p tabIndex="-1" className="grid col-span-2 font-bold">Previously</p>
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aCamPreviously-${aCamPreviously ?? 0}`}
               maxLength="3"
               defaultValue={aCamPreviously} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`bCamPreviously-${bCamPreviously ?? 0}`}
               maxLength="3"
               defaultValue={bCamPreviously} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`cCamPreviously-${cCamPreviously ?? 0}`}
               maxLength="3"
               defaultValue={cCamPreviously} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aSoundPreviously-${aSoundPreviously ?? 0}`}
               maxLength="3"
               defaultValue={aSoundPreviously} />
             <p className="grid col-span-2 font-bold">Today</p>
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aCamToday-${aCamToday ?? 0}`}
               maxLength="3" defaultValue={aCamToday} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`bCamToday-${bCamToday ?? 0}`}
               maxLength="3" defaultValue={bCamToday} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`cCamToday-${cCamToday ?? 0}`}
               maxLength="3" defaultValue={cCamToday} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aSoundToday-${aSoundToday ?? 0}`}
               maxLength="3" defaultValue={aSoundToday} />
 
             <p className="grid col-span-2 font-bold">To Date</p>
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aCamToDate-${aCamToDate ?? 0}`}
               maxLength="3" defaultValue={aCamToDate} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`bCamToDate-${bCamToDate ?? 0}`}
               maxLength="3" defaultValue={bCamToDate} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`cCamToDate-${cCamToDate ?? 0}`}
               maxLength="3" defaultValue={cCamToDate} />
             <TextInputField
+              tabIndex="-1"
               placeholder="0"
               key={`aSoundToDate-${aSoundToDate ?? 0}`}
               maxLength="3" defaultValue={aSoundToDate} />
