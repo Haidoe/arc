a79c8595b230d1eaa18c6a25ea9c81091d6cfc89
haidoe <haidren.amalia@gmail.com>
2023-07-22 10:21:18 -0700

    style: add padding bottom on pages with mobile menu

--- src/components/layouts/MainPageLayout.tsx
+++ src/components/layouts/MainPageLayout.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import type { FC, ReactNode } from "react";
 import Header from "~/components/global/Header.jsx";
 import MenuNavigationMobile from "~/components/global/MenuNavigationMobile.jsx";
@@ -5,9 +6,22 @@ import MenuNavigationMobile from "~/components/global/MenuNavigationMobile.jsx";
 type PageLayoutProp = { children: ReactNode; hideHeader?: boolean };
 
 const MainPageLayout: FC<PageLayoutProp> = ({ children, hideHeader }) => {
+  //This is our list of pages that we will disable padding because mobile nav is not visible
+  const disabledPages = ["/", "/sign-in"];
+
+  const router = useRouter();
+
+  const { pathname } = router;
+
+  const adtlClasses = disabledPages.includes(pathname)
+    ? ""
+    : "pb-[100px] lg:pb-0";
+
   return (
     <>
-      <div className="bg-base flex min-h-screen min-w-[320px] flex-col">
+      <div
+        className={`bg-base flex min-h-screen min-w-[320px] flex-col ${adtlClasses} `}
+      >
         {!hideHeader && <Header />}
 
         <main className="flex flex-1 flex-col">{children}</main>
