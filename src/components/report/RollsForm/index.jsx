7139dc92d2da21a27a85f1fb1f2d37e135ab23b6
JB Sinluenam <jb.sinluenam@gmail.com>
2023-07-21 17:07:28 -0700

    bug-fix-#156-rolls-form

--- src/components/report/RollsForm/index.jsx
+++ src/components/report/RollsForm/index.jsx
@@ -47,7 +47,7 @@ const RollsForm = ({ }) => {
     <>
       <Accordion title="Rolls" defaultOpen={true}>
         <div onClick={() => setIsOpen(true)} action="" className={` text-contrast-dark text-base font-bold`} >
-          <div className="pb-2 border-b  border-primary-base">
+          <div className="pb-4 border-b  border-primary-base">
             <div className="grid text-base text-bold grid-cols-6 grid-rows-2 gap-4 gap-y-2">
               <p tabIndex="-1" className="grid col-span-2">Script Supervisor</p>
               <TextInputField tabIndex="-1" className="grid col-span-4" placeholder="Please input the name." value={scriptSupervisor} />
@@ -56,80 +56,86 @@ const RollsForm = ({ }) => {
             </div>
           </div>
 
-          <div className=" text-contrast-dark grid grid-cols-6 grid-rows-4 gap-4 gap-y-4 pt-2">
-            <div className="grid col-span-2"></div>
-            <p tabIndex="-1" className="font-bold">A Cam</p>
-            <p tabIndex="-1" className="font-bold">B Cam</p>
-            <p tabIndex="-1" className="font-bold">C Cam</p>
-            <p tabIndex="-1" className="font-bold">A Sound</p>
-            <p tabIndex="-1" className="grid col-span-2 font-bold">Previously</p>
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aCamPreviously-${aCamPreviously ?? 0}`}
-              maxLength="3"
-              defaultValue={aCamPreviously} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`bCamPreviously-${bCamPreviously ?? 0}`}
-              maxLength="3"
-              defaultValue={bCamPreviously} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`cCamPreviously-${cCamPreviously ?? 0}`}
-              maxLength="3"
-              defaultValue={cCamPreviously} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aSoundPreviously-${aSoundPreviously ?? 0}`}
-              maxLength="3"
-              defaultValue={aSoundPreviously} />
-            <p className="grid col-span-2 font-bold">Today</p>
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aCamToday-${aCamToday ?? 0}`}
-              maxLength="3" defaultValue={aCamToday} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`bCamToday-${bCamToday ?? 0}`}
-              maxLength="3" defaultValue={bCamToday} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`cCamToday-${cCamToday ?? 0}`}
-              maxLength="3" defaultValue={cCamToday} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aSoundToday-${aSoundToday ?? 0}`}
-              maxLength="3" defaultValue={aSoundToday} />
+          <div className=" text-contrast-dark pt-4">
+            <div className="grid grid-cols-6 grid-rows-1 gap-4 gap-y-4 pb-2">
+              <div className="grid col-span-2"></div>
+              <p tabIndex="-1" className="font-bold">A Cam</p>
+              <p tabIndex="-1" className="font-bold">B Cam</p>
+              <p tabIndex="-1" className="font-bold">C Cam</p>
+              <p tabIndex="-1" className="font-bold">A Sound</p>
+            </div>
+            <div className="grid grid-cols-6 grid-rows-2 gap-4 pt-2 pb-4">
+              <p tabIndex="-1" className="grid col-span-2 font-bold">Previously</p>
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aCamPreviously-${aCamPreviously ?? 0}`}
+                maxLength="3"
+                defaultValue={aCamPreviously} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`bCamPreviously-${bCamPreviously ?? 0}`}
+                maxLength="3"
+                defaultValue={bCamPreviously} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`cCamPreviously-${cCamPreviously ?? 0}`}
+                maxLength="3"
+                defaultValue={cCamPreviously} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aSoundPreviously-${aSoundPreviously ?? 0}`}
+                maxLength="3"
+                defaultValue={aSoundPreviously} />
+              <p className="grid col-span-2 font-bold">Today</p>
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aCamToday-${aCamToday ?? 0}`}
+                maxLength="3" defaultValue={aCamToday} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`bCamToday-${bCamToday ?? 0}`}
+                maxLength="3" defaultValue={bCamToday} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`cCamToday-${cCamToday ?? 0}`}
+                maxLength="3" defaultValue={cCamToday} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aSoundToday-${aSoundToday ?? 0}`}
+                maxLength="3" defaultValue={aSoundToday} />
+            </div>
 
-            <p className="grid col-span-2 font-bold">To Date</p>
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aCamToDate-${aCamToDate ?? 0}`}
-              maxLength="3" defaultValue={aCamToDate} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`bCamToDate-${bCamToDate ?? 0}`}
-              maxLength="3" defaultValue={bCamToDate} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`cCamToDate-${cCamToDate ?? 0}`}
-              maxLength="3" defaultValue={cCamToDate} />
-            <TextInputField
-              tabIndex="-1"
-              placeholder="0"
-              key={`aSoundToDate-${aSoundToDate ?? 0}`}
-              maxLength="3" defaultValue={aSoundToDate} />
+            <div className="grid grid-cols-6 grid-rows-1 gap-4 py-4 border-t border-primary-base">
+              <p className="grid col-span-2 font-bold ">To Date</p>
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aCamToDate-${aCamToDate ?? 0}`}
+                maxLength="3" defaultValue={aCamToDate} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`bCamToDate-${bCamToDate ?? 0}`}
+                maxLength="3" defaultValue={bCamToDate} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`cCamToDate-${cCamToDate ?? 0}`}
+                maxLength="3" defaultValue={cCamToDate} />
+              <TextInputField
+                tabIndex="-1"
+                placeholder="0"
+                key={`aSoundToDate-${aSoundToDate ?? 0}`}
+                maxLength="3" defaultValue={aSoundToDate} />
+            </div>
           </div>
         </div >
       </Accordion>
